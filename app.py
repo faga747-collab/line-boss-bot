919852df471ec08c46aaf8a570c0c6d03700e06b
faga747-collab <faga747@gmail.com>
2026-04-17 02:35:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ tz = pytz.timezone('Asia/Taipei')
 # 🟢 Supabase PostgreSQL
 conn = psycopg2.connect(
     host=os.getenv("DB_HOST"),
+    port=os.getenv("DB_PORT"),
     database=os.getenv("DB_NAME"),
     user=os.getenv("DB_USER"),
     password=os.getenv("DB_PASSWORD")
